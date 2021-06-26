ee6e64486e1546d78ecb9ecccd884d37be5c25a5
Humed-Muhammad <abc45171@gmail.com>
2021-06-26 10:51:11 +0300

    pizza

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Login from "./components/Login/Login.jsx";
+import Home from "./components/Home/Home.jsx";
 
 function App() {
+  let [check, setCheck] = useState(false);
+
+  console.log(check);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {check ? <Home /> : <Login setCheck={setCheck} />}
     </div>
   );
 }
